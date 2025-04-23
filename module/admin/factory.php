f9fc2542f68814376b8cae923023b773e2711a34
Rakibul Karim Rakib <rakibul.rakib02@northsouth.edu>
2025-04-23 23:04:56 +0600

    Update factory.php

--- module/admin/factory.php
+++ module/admin/factory.php
@@ -1,9 +1,11 @@
 <?php
 
+// Define the common interface for locations
 interface Location {
     public function getDetails();
 }
 
+// Concrete classes for different locations
 class NACLocation implements Location {
     public function getDetails() {
         return "Booked for class purposes in NAC building.";
@@ -21,28 +23,32 @@ class LibLocation implements Location {
         return "Booked For Lab Purposes.";
     }
 }
+
 class AdminLocation implements Location {
     public function getDetails() {
         return "Booked For official Purposes.";
     }
 }
 
+// Factory class that creates different location types dynamically
 class LocationFactory {
+
+    // Mapping location types to their respective classes
+    private static $locationMap = [
+        'NAC' => NACLocation::class,
+        'SAC' => SACLocation::class,
+        'Library Building' => LibLocation::class,
+        'Admin Building' => AdminLocation::class,
+    ];
+
+    // Method to create a location dynamically based on the type
     public static function createLocation($type) {
-        switch ($type) {
-            case 'NAC':
-                return new NACLocation();
-            case 'SAC':
-                return new SACLocation();
-            case 'Library Buildin':
-                return new LibLocation();
-            case 'Admin Building':
-                return new AdminLocation();
-            default:
-                throw new Exception("Invalid location type.");
+        if (!array_key_exists($type, self::$locationMap)) {
+            throw new Exception("Invalid location type.");
         }
+        
+        // Instantiate the corresponding location class dynamically
+        return new self::$locationMap[$type]();
     }
 }
-
-
 ?>
